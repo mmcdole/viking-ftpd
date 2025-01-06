a8d60f40200cf75c0da9c851f4ef6768fc09b533
Matthew McDole <simucal@gmail.com>
2025-01-06 15:51:41 -0600

    Authentication fixing

--- pkg/authentication/authenticator.go
+++ pkg/authentication/authenticator.go
@@ -2,6 +2,7 @@ package authentication
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/mmcdole/viking-ftpd/pkg/users"
 )
@@ -23,17 +24,24 @@ func NewAuthenticator(source users.Source, verifier PasswordHashVerifier) *Authe
 // Authenticate verifies a username and password combination.
 // Returns ErrInvalidUsername if user not found, ErrInvalidPassword if password incorrect.
 func (a *Authenticator) Authenticate(username, password string) (*users.User, error) {
+	log.Printf("Authentication attempt for user: %s", username)
+	
 	user, err := a.source.LoadUser(username)
 	if err != nil {
 		if err == users.ErrUserNotFound {
+			log.Printf("User not found: %s", username)
 			return nil, ErrInvalidUsername
 		}
+		log.Printf("Error loading user %s: %v", username, err)
 		return nil, fmt.Errorf("loading user: %w", err)
 	}
 
+	log.Printf("Found user %s, verifying password (stored hash: %s)", username, user.PasswordHash)
 	if err := a.verifier.VerifyPassword(password, user.PasswordHash); err != nil {
+		log.Printf("Password verification failed for user %s: %v", username, err)
 		return nil, ErrInvalidPassword
 	}
 
+	log.Printf("Authentication successful for user: %s", username)
 	return user, nil
 }
